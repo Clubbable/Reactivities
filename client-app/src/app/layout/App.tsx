423c9dfe1bac8c75933b64aadf529d116272ccb5
Martin Liu <martinliu1993@hotmail.com>
2019-12-22 11:54:47 -0800

    added react router

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,29 +1,38 @@
-import { observer } from "mobx-react-lite";
-import React, { Fragment, useContext, useEffect } from "react";
-import { Container } from "semantic-ui-react";
-import ActivityDashboard from "../features/activities/dashboard/ActivityDashboard";
-import Navbar from "../features/nav/Navbar";
-import ActivityStore from "../stores/activityStore";
-import { LoadingComponent } from "./LoadingComponent";
+import React, { Fragment } from 'react';
+import { Container } from 'semantic-ui-react';
+import NavBar from '../../features/nav/NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import { observer } from 'mobx-react-lite';
+import { Route, withRouter, RouteComponentProps } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
-const App = () => {
-  const activityStore = useContext(ActivityStore);
+const App: React.FC<RouteComponentProps> = ({ location }) => {
 
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if (activityStore.loadingInitial)
-    return <LoadingComponent content="Loading activities..." />;
 
   return (
     <Fragment>
-      <Navbar />
-      <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard />
-      </Container>
+      <Route exact path='/' component={HomePage} />
+      <Route
+        path={'/(.+)'}
+        render={() => (
+          <Fragment>
+            <NavBar />
+            <Container style={{ marginTop: '7em' }}>
+              <Route exact path='/activities' component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetails} />
+              <Route
+                key={location.key}
+                path={['/createActivity', '/manage/:id']}
+                component={ActivityForm}
+              />
+            </Container>
+          </Fragment>
+        )}
+      />
     </Fragment>
   );
 };
 
-export default observer(App);
+export default withRouter(observer(App));
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,13 +1,13 @@
-import React, { useContext } from "react";
-import { Item, Button, Label, Segment } from "semantic-ui-react";
-import { observer } from "mobx-react-lite";
-import ActivityStore from "../../../stores/activityStore";
+import React, { useContext } from 'react';
+import { Item, Button, Label, Segment } from 'semantic-ui-react';
+import { observer } from 'mobx-react-lite';
+import ActivityStore from '../../../app/stores/activityStore';
+import { Link } from 'react-router-dom';
 
 const ActivityList: React.FC = () => {
   const activityStore = useContext(ActivityStore);
   const {
     activitiesByDate,
-    selectActivity,
     deleteActivity,
     submitting,
     target
@@ -18,7 +18,7 @@ const ActivityList: React.FC = () => {
         {activitiesByDate.map(activity => (
           <Item key={activity.id}>
             <Item.Content>
-              <Item.Header as="a">{activity.title}</Item.Header>
+              <Item.Header as='a'>{activity.title}</Item.Header>
               <Item.Meta>{activity.date}</Item.Meta>
               <Item.Description>
                 <div>{activity.description}</div>
@@ -28,20 +28,21 @@ const ActivityList: React.FC = () => {
               </Item.Description>
               <Item.Extra>
                 <Button
-                  onClick={() => selectActivity(activity.id)}
-                  floated="right"
-                  content="View"
-                  color="blue"
-                ></Button>
+                  as={Link}
+                  to={`/activities/${activity.id}`}
+                  floated='right'
+                  content='View'
+                  color='blue'
+                />
                 <Button
                   name={activity.id}
-                  onClick={e => deleteActivity(e, activity.id)}
                   loading={target === activity.id && submitting}
-                  floated="right"
-                  content="Delete"
-                  color="red"
-                ></Button>
-                <Label basic content={activity.category}></Label>
+                  onClick={e => deleteActivity(e, activity.id)}
+                  floated='right'
+                  content='Delete'
+                  color='red'
+                />
+                <Label basic content={activity.category} />
               </Item.Extra>
             </Item.Content>
           </Item>

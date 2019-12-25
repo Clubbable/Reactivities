8c6579324d0250cfe57f4a1e3d8f1f233e0f1ae6
Martin Liu <martinliu1993@hotmail.com>
2019-12-25 12:32:58 -0800

    implemented react final form and proper date pickers

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,8 +1,8 @@
-import React, { useContext, Fragment } from "react";
-import { Item, Label } from "semantic-ui-react";
-import { observer } from "mobx-react-lite";
-import ActivityStore from "../../../app/stores/activityStore";
-import { ActivityListItem } from "./ActivityListItem";
+import React, { useContext, Fragment } from 'react';
+import { Item, Label } from 'semantic-ui-react';
+import { observer } from 'mobx-react-lite';
+import ActivityStore from '../../../app/stores/activityStore';
+import ActivityListItem from './ActivityListItem';
 
 const ActivityList: React.FC = () => {
   const activityStore = useContext(ActivityStore);
@@ -11,15 +11,12 @@ const ActivityList: React.FC = () => {
     <Fragment>
       {activitiesByDate.map(([group, activities]) => (
         <Fragment key={group}>
-          <Label size="large" color="blue">
+          <Label size='large' color='blue'>
             {group}
           </Label>
           <Item.Group divided>
             {activities.map(activity => (
-              <ActivityListItem
-                key={activity.id}
-                activity={activity}
-              ></ActivityListItem>
+              <ActivityListItem key={activity.id} activity={activity} />
             ))}
           </Item.Group>
         </Fragment>
